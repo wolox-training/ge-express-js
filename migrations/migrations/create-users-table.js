a0dbb887c99ab0b2ec83b4284dc86a1698093646
Gon <gonzalo.escalante@wolox.com.ar>
2019-06-09 21:22:03 -0300

    fixed migration

--- migrations/migrations/create-users-table.js
+++ migrations/migrations/create-users-table.js
@@ -1,6 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Users', {
+    queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
       name: {
         type: Sequelize.STRING,
         allowNull: false
@@ -16,7 +22,15 @@ module.exports = {
       password: {
         type: Sequelize.STRING,
         allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     }),
-  down: queryInterface => queryInterface.dropTable('Users')
+  down: queryInterface => queryInterface.dropTable('users')
 };
