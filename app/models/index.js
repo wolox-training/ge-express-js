c26c56f9240740043dbf42c886629cc8c8a96b9c
Gon <gonzalo.escalante@wolox.com.ar>
2019-06-13 10:32:49 -0300

    added heroku sequelize connection

--- app/models/index.js
+++ app/models/index.js
@@ -6,7 +6,9 @@ const fs = require('fs'),
   dbConfig = require('../../config/db')[config.environment],
   db = {};
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL)
+  : new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
